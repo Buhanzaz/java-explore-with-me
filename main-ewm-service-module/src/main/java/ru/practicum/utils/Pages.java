21e867134f9d24a1f046e1f349289ac58874a4d1
Eugene Bukhanov <buhanza21@yandex.ru>
2024-01-10 03:51:59 +0300

    refactor: changed sorting

--- main-ewm-service-module/src/main/java/ru/practicum/utils/Pages.java
+++ main-ewm-service-module/src/main/java/ru/practicum/utils/Pages.java
@@ -6,11 +6,11 @@ import org.springframework.data.domain.Sort;
 
 public final class Pages {
     public static Pageable getPageForUsers(Integer from, Integer size) {
-        return PageRequest.of(from / size, size, Sort.by(Sort.Direction.DESC, "id"));
+        return PageRequest.of(from / size, size, Sort.by(Sort.Direction.ASC, "id"));
     }
 
     public static Pageable getPageForCategories(Integer from, Integer size) {
-        return PageRequest.of(from / size, size, Sort.by(Sort.Direction.ASC, "name"));
+        return PageRequest.of(from / size, size, Sort.by(Sort.Direction.ASC, "id"));
     }
 
     public static Pageable getPageForEvents(Integer from, Integer size) {
