f0a9f28205e4e63bed5a9515d09d33aedd52243e
Buhanzaz <Buhanza@gmail.com>
2024-01-11 16:42:42 +0300

    refactor: changed name dto

--- statistics-module/stats-dto/src/main/java/ru/practicum/dto/ViewStats.java
+++ statistics-module/stats-dto/src/main/java/ru/practicum/dto/ViewStats.java
@@ -8,7 +8,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ResponseStatisticDto {
+public class ViewStats {
     String app;
     String uri;
     Long hits;
