24438cd48e7d89cc1e4cf4d42e07655a82687398
Buhanzaz <Buhanza@gmail.com>
2024-01-11 16:43:10 +0300

    refactor: delete client controller
    refactor: delete client controller

--- statistics-module/stats-client/src/main/java/ru/practicum/client/ClientStatisticsImplWeb.java
+++ statistics-module/stats-client/src/main/java/ru/practicum/client/ClientStatisticsImplWeb.java
@@ -7,10 +7,12 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.client.web.WebBaseClient;
-import ru.practicum.dto.ResponseStatisticDto;
-import ru.practicum.dto.StatisticDto;
+import ru.practicum.dto.EndpointHit;
+import ru.practicum.dto.ViewStats;
+import static ru.practicum.variables.StaticVariables.FORMATTER;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -18,24 +20,25 @@ import java.util.Map;
 @Component
 class ClientStatisticsImplWeb extends WebBaseClient implements ClientStatistics {
 
-    public ClientStatisticsImplWeb(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public ClientStatisticsImplWeb(@Value("${stats.server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new).build());
     }
 
     @Override
-    public StatisticDto addStatsInfo(StatisticDto dto) {
-        return post(dto, new StatisticDto()).getBody();
+    public EndpointHit addStatsInfo(EndpointHit dto) {
+        return post(dto, new EndpointHit()).getBody();
     }
 
     @Override
-    public List<ResponseStatisticDto> statisticsOutput(LocalDateTime start, LocalDateTime end, String[] uris, boolean unique) {
-        Map<String, Object> params = Map.of("start", start,
-                "end", end,
+    public List<ViewStats> statisticsOutput(LocalDateTime start, LocalDateTime end, String[] uris, boolean unique) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMATTER);
+        Map<String, Object> params = Map.of("start", start.format(formatter),
+                "end", end.format(formatter),
                 "uris", uris,
                 "unique", unique);
 
-        ResponseEntity<ArrayList<ResponseStatisticDto>> response = get(params, new ArrayList<>());
+        ResponseEntity<ArrayList<ViewStats>> response = get(params, new ArrayList<>());
 
         return response.getBody();
     }
